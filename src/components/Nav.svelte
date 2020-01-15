0e5dc666b8741124aa81a9fd1d13bd250afd7848
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2020-01-15 14:20:38 +0530

    feat(): adds sidebar UI :lipstick:

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,61 @@
 <script>
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
+  nav {
+    background: #24cea3;
+    width: 270px;
+    height: 100vh;
+    position: fixed;
+    left: 0;
+    font-weight: 300;
+  }
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
+  ul {
+    margin: 0;
+    padding: 0;
+  }
 
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+  /* clearfix */
+  ul::after {
+    content: "";
+    display: block;
+    clear: both;
+  }
 
-	li {
-		display: block;
-		float: left;
-	}
+  li {
+    display: flex;
+  }
 
-	.selected {
-		position: relative;
-		display: inline-block;
-	}
+  .selected {
+    position: relative;
+    display: inline-block;
+    background: #1ea97f;
+  }
 
-	.selected::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+  a {
+    text-decoration: none;
+    padding: 1em 0.5em;
+    display: block;
+    width: 100%;
+    color: white;
+  }
 </style>
 
 <nav>
-	<ul>
-		<li><a class:selected='{segment === undefined}' href='.'>home</a></li>
-		<li><a class:selected='{segment === "about"}' href='about'>about</a></li>
+  <ul>
+    <li>
+      <a class:selected={segment === undefined} href=".">home</a>
+    </li>
+    <li>
+      <a class:selected={segment === 'about'} href="about">about</a>
+    </li>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>
-	</ul>
+    <li>
+      <a rel="prefetch" class:selected={segment === 'blog'} href="blog">blog</a>
+    </li>
+  </ul>
 </nav>
